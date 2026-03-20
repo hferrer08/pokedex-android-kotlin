f951fb16b4498ba0bb99901986095bf5fbc52489
hferrer08 <hubert.ferrerg8@gmail.com>
2026-03-20 09:21:16 -0300

    Creación del proyecto base PokeDex151 con Android Studio, Kotlin y Jetpack Compose

---
+++ app/src/main/java/com/hferrer08/pokedex151/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.hferrer08.pokedex151
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.hferrer08.pokedex151.ui.theme.PokeDex151Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            PokeDex151Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    PokeDex151Theme {
+        Greeting("Android")
+    }
+}
