f44a26b20e4cd8e3d4c0ab1a6719a076571c3b23
hferrer08 <hubert.ferrerg8@gmail.com>
2026-03-21 17:20:58 -0300

    Agregar navegación de compose para detallado

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,7 @@ dependencies {
     implementation(libs.retrofit)
     implementation(libs.converter.gson)
     implementation(libs.coil.compose)
+    implementation(libs.androidx.navigation.compose)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
