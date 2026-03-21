669ad646dd4789c36d8a4a9babb837e4b86c8679
hferrer08 <hubert.ferrerg8@gmail.com>
2026-03-21 11:29:10 -0300

    Se quitan greetings default

--- app/src/main/java/com/hferrer08/pokedex151/MainActivity.kt
+++ app/src/main/java/com/hferrer08/pokedex151/MainActivity.kt
@@ -30,18 +30,4 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PokeDex151Theme {
-        Greeting("Android")
-    }
-}
